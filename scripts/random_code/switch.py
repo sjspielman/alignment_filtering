ab398e9efa525c533ed6abe260bfbf7e8da25400
Stephanie <stephanie.spielman@gmail.com>
2013-06-24 14:23:23 -0500

    Deleting scripts due to poor commenting.

--- scripts/random_code/switch.py
+++
@@ -1,8 +0,0 @@
-
-
-bases=['shortindel_16p', 'medindel_16p', 'bigindel_16p', 'shortindel_16b', 'medindel_16b', 'bigindel_16b', 'shortindel_16r', 'medindel_16r', 'bigindel_16r', 'shortindel_16rc', 'medindel_16rc', 'bigindel_16rc', 'shortindel_64p', 'medindel_64p', 'bigindel_64p', 'shortindel_64b', 'medindel_64b', 'bigindel_64b', 'shortindel_64r', 'medindel_64r', 'bigindel_64r', 'shortindel_64rc', 'medindel_64rc', 'bigindel_64rc']
-
-for n in range(1,len(bases)):
-	command="sed -i 's/"+bases[n-1]+"/"+bases[n]+"/g' truefubar.qsub"
-	print command
-	print "qsub truefubar.qsub"
