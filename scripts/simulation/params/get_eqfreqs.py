b71db0b5502415e43f68acfb8d317f83e225045b
sjspielman <stephanie.spielman@utexas.edu>
2014-04-02 12:30:15 -0500

    Will now output codon frequencies in the format indelible wants

--- scripts/simulation/params/get_eqfreqs.py
+++ scripts/simulation/params/get_eqfreqs.py
@@ -7,6 +7,7 @@ from Bio import SeqIO, AlignIO
 from Bio.Align import MultipleSeqAlignment
 import numpy
 
+
 file = sys.argv[1]
 
 codon_dict = {"TTT":0, "TTC":0, "TTA":0, "TTG":0, "TCT":0, "TCC":0, "TCA":0, "TCG":0, "TAT":0, "TAC":0, "TAA":0, "TAG":0, "TGT":0, "TGC":0, "TGA":0, "TGG":0, "CTT":0, "CTC":0, "CTA":0, "CTG":0, "CCT":0, "CCC":0, "CCA":0, "CCG":0, "CAT":0, "CAC":0, "CAA":0, "CAG":0, "CGT":0, "CGC":0, "CGA":0, "CGG":0, "ATT":0, "ATC":0, "ATA":0, "ATG":0, "ACT":0, "ACC":0, "ACA":0, "ACG":0, "AAT":0, "AAC":0, "AAA":0, "AAG":0, "AGT":0, "AGC":0, "AGA":0, "AGG":0, "GTT":0, "GTC":0, "GTA":0, "GTG":0, "GCT":0, "GCC":0, "GCA":0, "GCG":0, "GAT":0, "GAC":0, "GAA":0, "GAG":0, "GGT":0, "GGC":0, "GGA":0, "GGG":0}
@@ -25,10 +26,25 @@ while end <= len(seq) :
 	start+=3
 	end+=3
 
+
+sum=0
+
+indelible_order = ['T', 'C', 'A', 'G']
+four_codons = []
 numcodon=len(seq)/3
-total=0
-for entry in codon_dict:
-	print entry, float(codon_dict[entry])/numcodon
+for a in range(4):
+	codon1 = indelible_order[a]
+	for b in range(4):
+		codon2 = codon1 + indelible_order[b]
+		for c in range(4):
+			codon = codon2 + indelible_order[c]
+			value =float(codon_dict[codon])/numcodon 
+			sum += value
+			four_codons.append(value)
+		print four_codons[0], four_codons[1], four_codons[2], four_codons[3]
+		four_codons=[]
+
+print sum ## For GP41, sum=0.998028985507. Indelible will rescale this. I'm comfortable with this being basically 1 due to rounding errors.
 
 '''
 HA:
