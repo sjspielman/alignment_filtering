9033cb6da7f329d010c33414eded49235d8c7411
sjspielman <stephanie.spielman@utexas.edu>
2014-04-02 14:18:14 -0500

    sed script for paml submissions. do not run flat!!

--- scripts/cluster/submit_paml.sh
+++ scripts/cluster/submit_paml.sh
@@ -1,24 +1,150 @@
-######## Submission script for paml jobs, via paml.qsub only. Python script no longer needed.
+######## Submission script for paml jobs, via $QSUB only. Python script no longer needed.
+## DO NOT RUN FLAT WITH PAML!!
 
-## For now, we are just starting out with rho neutral. Just run all algs with 50 mask and rho fixed.
 
 ## Starting conditions:
-#GENE=rho
+#GENE=or5
 #MASK=50
-#ALG=Guidance
+#ALG=refaln
 
-qsub paml.qsub
-sed -i 's/ALG=Guidance/ALG=GuidanceP/g' paml.qsub
-qsub paml.qsub
-sed -i 's/ALG=GuidanceP/ALG=BMweights/g' paml.qsub
-qsub paml.qsub
-sed -i 's/ALG=BMweights/ALG=BMweightsP/g' paml.qsub
-qsub paml.qsub
-sed -i 's/ALG=BMweightsP/ALG=PDweights/g' paml.qsub
-qsub paml.qsub
-sed -i 's/ALG=PDweights/ALG=PDweightsP/g' paml.qsub
-qsub paml.qsub
-sed -i 's/ALG=PDweightsP/ALG=refaln/g' paml.qsub
-qsub paml.qsub
+QSUB=/home/sjs3495/alignment_filtering/scripts/cluster/fubar.qsub
 
-sed -i 's/ALG=refaln/ALG=Guidance/g' paml.qsub
+## Run reference alignment
+qsub $QSUB
+
+####### Run or5
+sed -i 's/ALG=refaln/ALG=Guidance/g'
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=BMweights/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=BMweights/ALG=BMweightsP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=BMweightsP/ALG=PDweights/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=PDweights/ALG=PDweightsP/g' $QSUB
+qsub $QSUB
+
+######## Reset algorithm and change gene
+sed -i 's/ALG=PDweightsP/ALG=Guidance/g' $QSUB
+sed -i 's/GENE=or5/GENE=rho/g' $QSUB
+
+######## Run rho
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=BMweights/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=BMweights/ALG=BMweightsP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=BMweightsP/ALG=PDweights/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=PDweights/ALG=PDweightsP/g' $QSUB
+qsub $QSUB
+
+######## Reset algorithm and change gene
+sed -i 's/ALG=PDweightsP/ALG=Guidance/g' $QSUB
+sed -i 's/GENE=rho/GENE=prk/g' $QSUB
+
+######## Run prk
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=BMweights/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=BMweights/ALG=BMweightsP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=BMweightsP/ALG=PDweights/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=PDweights/ALG=PDweightsP/g' $QSUB
+qsub $QSUB
+
+
+################### RUN GUIDANCE(P) FOR 30,70,90 MASKS ########################
+
+## Reset algorithm to Guidance
+sed -i 's/ALG=PDweightsP/ALG=Guidance/g' $QSUB
+
+######### MASK 30
+sed -i 's/MASK=50/MASK=30/g' $QSUB
+
+## or5 
+sed -i 's/GENE=prk/GENE=or5/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=Guidance/g' $QSUB
+
+#rho
+sed -i 's/GENE=or5/GENE=rho/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=Guidance/g' $QSUB
+
+#prk
+sed -i 's/GENE=rho/GENE=prk/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=Guidance/g' $QSUB
+
+
+######### MASK 70
+sed -i 's/MASK=30/MASK=70/g' $QSUB
+
+## or5 
+sed -i 's/GENE=prk/GENE=or5/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=Guidance/g' $QSUB
+
+#rho
+sed -i 's/GENE=or5/GENE=rho/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=Guidance/g' $QSUB
+
+#prk
+sed -i 's/GENE=rho/GENE=prk/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=Guidance/g' $QSUB
+
+
+######### MASK 90
+sed -i 's/MASK=70/MASK=90/g' $QSUB
+
+## or5 
+sed -i 's/GENE=prk/GENE=or5/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=Guidance/g' $QSUB
+
+#rho
+sed -i 's/GENE=or5/GENE=rho/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=Guidance/g' $QSUB
+
+#prk
+sed -i 's/GENE=rho/GENE=prk/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=Guidance/ALG=GuidanceP/g' $QSUB
+qsub $QSUB
+sed -i 's/ALG=GuidanceP/ALG=Guidance/g' $QSUB
+
+
+########################### FULL RESET TO INITIAL CONDITIONS #############################
+
+sed -i 's/MASK=90/MASK=50/g' $QSUB
+sed -i 's/GENE=prk/GENE=or5/g' $QSUB
+sed -i 's/ALG=Guidance/ALG=refaln/g' $QSUB
+
+
