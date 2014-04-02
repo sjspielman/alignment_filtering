18f92a863f71a2802bbd905c85d0a2f60a8642ef
sjspielman <stephanie.spielman@utexas.edu>
2014-04-02 14:12:33 -0500

    Submission files for truealn inference

---
+++ scripts/cluster/submit_true.sh
@@ -0,0 +1,74 @@
+######## Submission script for all truealn jobs, both fubar and paml. ##########
+
+## Starting conditions:
+#DATASET=HA
+#GENE=or5
+
+QSUB_FU=/home/sjs3495/alignment_filtering/scripts/cluster/truefu.qsub
+QSUB_PAML=/home/sjs3495/alignment_filtering/scripts/cluster/truepaml.qsub
+
+######## Run all genes for HA, then all genes for GP41 ########
+
+###################################### HA ################################################
+# HA, or5
+qsub $QSUB_FU
+qsub $QSUB_PAML
+
+# HA, rho
+sed -i 's/GENE=or5/GENE=rho/g' $QSUB_FU
+sed -i 's/GENE=or5/GENE=rho/g' $QSUB_PAML
+qsub $QSUB_FU
+qsub $QSUB_PAML
+
+# HA, prk
+sed -i 's/GENE=rho/GENE=prk/g' $QSUB_FU
+sed -i 's/GENE=rho/GENE=prk/g' $QSUB_PAML
+qsub $QSUB_FU
+qsub $QSUB_PAML
+
+# HA, flat
+sed -i 's/GENE=prk/GENE=flat/g' $QSUB_FU
+sed -i 's/GENE=prk/GENE=flat/g' $QSUB_PAML
+qsub $QSUB_FU
+qsub $QSUB_PAML
+##########################################################################################
+
+
+## RESET TO OR5 AND SWAP DATASET TO GP41
+sed -i 's/GENE=flat/GENE=or5/g' $QSUB_FU
+sed -i 's/GENE=flat/GENE=or5/g' $QSUB_PAML
+sed -i 's/DATASET=HA/DATASET=GP41/g' $QSUB_FU
+sed -i 's/DATASET=HA/DATASET=GP41/g' $QSUB_PAML
+
+##################################### GP41 ###############################################
+
+# GP41, or5
+qsub $QSUB_FU
+qsub $QSUB_PAML
+
+# GP41, rho
+sed -i 's/GENE=or5/GENE=rho/g' $QSUB_FU
+sed -i 's/GENE=or5/GENE=rho/g' $QSUB_PAML
+qsub $QSUB_FU
+qsub $QSUB_PAML
+
+# GP41, prk
+sed -i 's/GENE=rho/GENE=prk/g' $QSUB_FU
+sed -i 's/GENE=rho/GENE=prk/g' $QSUB_PAML
+qsub $QSUB_FU
+qsub $QSUB_PAML
+
+# GP41, flat
+sed -i 's/GENE=prk/GENE=flat/g' $QSUB_FU
+sed -i 's/GENE=prk/GENE=flat/g' $QSUB_PAML
+qsub $QSUB_FU
+qsub $QSUB_PAML
+##########################################################################################
+
+
+#### RESET TO STARTING CONDITIONS
+sed -i 's/GENE=flat/GENE=or5/g' $QSUB_FU
+sed -i 's/GENE=flat/GENE=or5/g' $QSUB_PAML
+sed -i 's/DATASET=GP41/DATASET=HA/g' $QSUB_FU
+sed -i 's/DATASET=GP41/DATASET=HA/g' $QSUB_PAML
+
